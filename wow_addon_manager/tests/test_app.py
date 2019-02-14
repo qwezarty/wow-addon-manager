6eb13fc8286c5f596a4e6db02119df8bd7408bb8
qwezarty <qwezarty@gmail.com>
2019-02-14 15:24:32 +0800

    fix test init dir failed

--- wow_addon_manager/tests/test_app.py
+++ wow_addon_manager/tests/test_app.py
@@ -9,21 +9,14 @@ root_path = Path(__file__).parent.parent
 app = App(__name__, root_path)
 app.system_config['run_mode'] == 'test'
 
-def test_init_dirs():
+def test_init_install_dirs():
     interface_dir = path.join(root_path, 'tests', 'dst', 'Interface')
     app.addons_dir = path.join(interface_dir, 'Addons')
-    cache_path = path.join(root_path, 'cache')
-    if path.exists(cache_path):
-        rmtree(cache_path)
     if path.exists(interface_dir):
         rmtree(interface_dir)
 
-    app.init_dirs()
-
+    app.init_install_dirs()
     assert path.exists(app.addons_dir), 'addons folder should be exist after init dirs.'
-    assert path.exists(cache_path), 'cache folder should be exist after init dirs.'
-
-    rmtree(cache_path)
     rmtree(interface_dir)
 
 def test_get_user_config_path():
